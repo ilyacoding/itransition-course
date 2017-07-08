7f8ecb0f4399659c57a4c506db69db36bb09cc27
Ilya Kovalenko <ilya@ilyakovalenko.com>
2017-07-08 21:49:41 +0300

    lab6 on my way

---
+++ RubymineProjects/task2/devby.rb
@@ -0,0 +1,13 @@
+require_relative "devby_registrator.rb"
+
+if ARGV.length.zero?
+  puts "Too few arguments"
+  exit
+end
+puts "Login Password"
+object = DevbyRegistrator.new
+
+ARGV[0].to_i.times do
+  user = object.register
+  puts "#{user[:login]} #{user[:password]}"
+end
---
+++ RubymineProjects/task2/devby_registrator.rb
@@ -0,0 +1,73 @@
+require 'rubygems'
+require 'capybara'
+require 'capybara/dsl'
+require 'json'
+require 'net/http'
+require 'uri'
+require 'securerandom'
+
+class DevbyRegistrator
+  include Capybara::DSL
+
+  def initialize
+    Capybara.current_driver = :selenium
+  end
+
+  def create_email
+    uri = URI.parse("https://post-shift.ru/api.php?action=new&type=json")
+    response = Net::HTTP.get_response(uri)
+    JSON.parse(response.body)
+  end
+
+  def delete_email(key)
+    uri = URI.parse("https://post-shift.ru/api.php?action=delete&key=#{key}")
+    response = Net::HTTP.get_response(uri)
+    if response.body == "OK"
+      true
+    else
+      false
+    end
+  end
+
+  def wait_email(key)
+    while true
+      uri = URI.parse("https://post-shift.ru/api.php?action=getlist&key=#{key}&type=json")
+      response = Net::HTTP.get_response(uri)
+      list = JSON.parse(response.body)
+      break if list.is_a?(Array)
+    end
+  end
+
+  def confirm(key)
+    uri = URI.parse("https://post-shift.ru/api.php?action=getmail&key=#{key}&id=1")
+    response = Net::HTTP.get_response(uri)
+    link = get_link(response.body.to_s)
+    visit(link.to_s)
+  end
+
+  def get_link(str)
+    str.match(/< ?([^>]+) ?>\Z/)[1].sub("=3D", "=")
+  end
+
+  def register
+    while true
+      login = SecureRandom.hex(8)
+      password = SecureRandom.hex(8)
+      visit("http://dev.by/registration")
+      email_credentials = create_email()
+      fill_in('user[username]', with: login)
+      fill_in('user[email]', with: email_credentials['email'])
+      fill_in('user[password]', with: password)
+      fill_in('user[password_confirmation]', with: password)
+      check("user_agreement")
+      find(".blue.btn.submit").click
+      break if page.html.include? "icon-dev-a-success"
+      delete_email(email_credentials['key'])
+    end
+
+    wait_email(email_credentials['key'])
+    confirm(email_credentials['key'])
+    delete_email(email_credentials['key'])
+    { :login => login, :password => password }
+  end
+end
